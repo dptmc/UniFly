db351caed5d6f669e9ce29a42b3269470ab2016a
DartPower <>
2020-12-22 18:57:13 +0300

    Init

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# DPT-PluginRepoTemplate
-DPT Specific Plugins for Bukkit-based plugin template
+# UniFly
+
+UniFly
 
 ![Build](https://github.com/dptmc/DPT-PluginRepoTemplate/workflows/Build/badge.svg)
---
+++ src/main/java/io/dpteam/UniFly/Main.java
@@ -0,0 +1,77 @@
+package io.dpteam.UniFly;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UniFly extends JavaPlugin implements Listener {
+	private List enabledWorlds = new ArrayList();
+	private List flyCommands = new ArrayList();
+	private String flyNotAllowed = "";
+	private Permission bypassPermission = new Permission("unifly.bypass");
+
+	public UniFly() {
+		super();
+	}
+
+	public void onEnable() {
+		this.loadConfig();
+		this.getServer().getPluginManager().addPermission(this.bypassPermission);
+		this.getServer().getPluginManager().registerEvents(this, this);
+		Bukkit.getServer().getLogger().info("[DPT.MC] UniFly Enabled");
+	}
+
+	public void onDisable() {
+		this.getServer().getPluginManager().removePermission(this.bypassPermission);
+		Bukkit.getServer().getLogger().info("[DPT.MC] UniFly Disabled");
+	}
+
+	public void loadConfig() {
+		this.getConfig().addDefault("Enabled worlds", Arrays.asList("plots"));
+		this.getConfig().addDefault("Fly commands", Arrays.asList("fly"));
+		this.getConfig().addDefault("Messages.Cannot fly", "&cYou cannot fly in this world!");
+		this.getConfig().options().copyDefaults(true);
+		this.saveConfig();
+		this.enabledWorlds = this.getConfig().getStringList("Enabled worlds");
+		this.flyCommands = this.getConfig().getStringList("Fly commands");
+		this.flyNotAllowed = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("Messages.Cannot fly"));
+	}
+
+	@EventHandler
+	public void onPlayerChangeWorld(PlayerChangedWorldEvent event) {
+		try {
+			if (!event.getPlayer().hasPermission(this.bypassPermission) && this.enabledWorlds.contains(event.getFrom().getName()) && !this.enabledWorlds.contains(event.getPlayer().getWorld().getName())) {
+				event.getPlayer().setAllowFlight(false);
+			}
+		} catch (Exception var3) {
+			var3.printStackTrace();
+		}
+	}
+
+	@EventHandler
+	public void onPlayerTypeFly(PlayerCommandPreprocessEvent event) {
+		try {
+			if (event.isCancelled()) {
+				return;
+			}
+
+			if (!event.getPlayer().hasPermission(this.bypassPermission) && !event.getPlayer().getAllowFlight() && !this.enabledWorlds.contains(event.getPlayer().getWorld().getName())) {
+				String command = event.getMessage().contains(" ") ? event.getMessage().split(" ")[0] : event.getMessage();
+				command = command.replaceFirst("/", "");
+				if (this.flyCommands.contains(command.toLowerCase())) {
+					event.getPlayer().sendMessage(this.flyNotAllowed.replace("<name>", event.getPlayer().getName()));
+					event.setCancelled(true);
+				}
+			}
+		} catch (Exception var3) {
+			var3.printStackTrace();
+		}
+	}
+}
